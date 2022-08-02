c4ea4fbb2db2838eed37ada499889251fcef5b4f
Kim Wauben <72084242+KimW1998@users.noreply.github.com>
2022-08-02 14:20:47 +0200

    extracted the token

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,28 +1,35 @@
-import { useState } from 'react';
-import AuthContent from '../components/Auth/AuthContent';
-import { createUser } from '../util/auth';
-import LoadingOverlay from '../components/ui/LoadingOverlay';
-import { Alert } from 'react-native';
+import { useContext, useState } from "react";
+import AuthContent from "../components/Auth/AuthContent";
+import { createUser } from "../util/auth";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
+import { Alert } from "react-native";
+import { AuthContext } from "../store/auth-context";
 
 function SignupScreen() {
   const [isAuthenticating, setIsAuthenticating] = useState(false);
 
-  async function signupHandler({email, password}) {
+  const authCtx = useContext(AuthContext);
+
+  async function signupHandler({ email, password }) {
     setIsAuthenticating(true);
     try {
-      await createUser(email, password);
+     const token = await createUser(email, password);
+      authCtx.authenticate(token);
     } catch (error) {
-      Alert.alert('Authentication failed', 'Could not create user, please check your input and try again later');
+      Alert.alert(
+        "Authentication failed",
+        "Could not create user, please check your input and try again later"
+      );
     }
-   
-   setIsAuthenticating(false);
+
+    setIsAuthenticating(false);
   }
 
   if (isAuthenticating) {
-    return <LoadingOverlay message="creating user...."/>;
+    return <LoadingOverlay message="creating user...." />;
   }
 
-  return <AuthContent onAuthenticate={signupHandler}/>;
+  return <AuthContent onAuthenticate={signupHandler} />;
 }
 
 export default SignupScreen;
