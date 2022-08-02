173a3f42a939225434e816304d0664a4319efbe8
Kim Wauben <72084242+KimW1998@users.noreply.github.com>
2022-08-02 15:01:42 +0200

    fetch a basic text from database and place in welcome screen

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,10 +1,25 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { ActionSheetIOS, StyleSheet, Text, View } from "react-native";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function WelcomeScreen() {
+  const [fetchedMessage, setFetchedMessage] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://react-native-course-71f95-default-rtdb.europe-west1.firebasedatabase.app/message.json"
+      )
+      .then((response) => {
+        setFetchedMessage(response.data);
+      });
+  }, []);
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{fetchedMessage}</Text>
     </View>
   );
 }
@@ -14,13 +29,13 @@ export default WelcomeScreen;
 const styles = StyleSheet.create({
   rootContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     padding: 32,
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 8,
   },
 });
